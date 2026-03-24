d732c337efa7378d962451f1ba5a93b646263c72
Mostafa12966 <fobyaff620@gmail.com>
2026-03-24 20:17:06 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 document.getElementById("btn").addEventListener("click",function(m){
 
 m.preventDefault();
@@ -8,7 +6,7 @@ m.preventDefault();
 
 let phone=document.getElementById("phone").value;
 let email=document.getElementById("email").value;
-let Masseage=document.getElementById("Masseage ").value;
+let Masseage=document.getElementById("Masseage").value;
 let isValid=true
 
 // //////////////////////////////////////////
@@ -29,43 +27,44 @@ document.getElementById("p").innerHTML=""
 
 
 if (phone === "" || email === "" || Masseage === "") {
-    document.getElementById("p").innerHTML="please inter Your data"
+    document.getElementById("p").innerHTML="please enter Your data"
     return;
 }
 
 if(isNaN(phone) || phone.length>12){
-document.getElementById("p1").innerHTML="not avalid number"
+document.getElementById("p1").innerHTML="not a valid number"
 document.getElementById("phone").style.border="2px solid red";
-
+isValid = false;
 }
 if(!email.includes("@") || !email.includes(".")){
-document.getElementById("p2").innerHTML="not avalid email"
+document.getElementById("p2").innerHTML="not a valid email"
 document.getElementById("email").style.border="2px solid red";
-
+isValid = false;
 }
 if(Masseage.length>50){
-document.getElementById("p3").innerHTML="Dont Be more than 50 letter"
+document.getElementById("p3").innerHTML="Don't be more than 50 letters"
 document.getElementById("Masseage").style.border="2px solid red";
-
+isValid = false;
 }
 if (isValid) {
     document.getElementById("p").innerHTML="All data is Correct"
 }
+}
     
-})
+)
 
-let btn = document.getElementById("scrollTopBtn");
+let btn1 = document.getElementById("scrollTopBtn");
 
 window.onscroll = function () {
   if (document.documentElement.scrollTop > 200) {
-    btn.style.display = "block";
+    btn1.style.display = "block";
   } else {
-    btn.style.display = "none";
+    btn1.style.display = "none";
   }
 };
 
 
-btn.onclick = function () {
+btn1.onclick = function () {
   window.scrollTo({
     top: 0,
     behavior: "smooth"
